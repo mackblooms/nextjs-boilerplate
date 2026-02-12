aff038e7acd9412ab917df4a13e45a1bfea46f59
mackblooms <mackbloom3@gmail.com>
2026-02-12 14:11:24 -0600

    Update page.tsx

--- app/pool/[id]/leaderboard/page.tsx
+++ app/pool/[id]/leaderboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { supabase } from "../../../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 type Row = {
   user_id: string;
