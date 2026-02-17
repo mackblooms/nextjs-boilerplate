e21720b80c437255401bc3ab8389d00de8a179a5
mackblooms <mackbloom3@gmail.com>
2026-02-17 15:13:12 -0600

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -2,7 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseAdmin() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
-  return createClient(url, key);
+  const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+  return createClient(url, serviceRole, {
+    auth: { persistSession: false },
+  });
 }
